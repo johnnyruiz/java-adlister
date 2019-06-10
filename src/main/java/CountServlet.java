7374feb922a5ef45b9df5239762d829e399493a6
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-06-10 16:16:59 -0500

    pushing up bonuses

---
+++ src/main/java/CountServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CountServlet", urlPatterns = "/count")
+public class CountServlet extends HttpServlet {
+         private static Integer count = 0;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         count = 0;
+         response.sendRedirect("/count");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        count += 1;
+        out.println("Count " + count);
+        response.setContentType("text/html");
+        out.println("<form method='Post'action=''><button>Reset</button>");
+    }
+}
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,25 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
 
+        String name = req.getParameter("name");
+
+        Integer counter = 0;
+
+        if(name != null){
+            out.println("Hello, " + name);
+        } else{
+            out.println("Hello, World");
+        }
+        
     }
+    
 }
