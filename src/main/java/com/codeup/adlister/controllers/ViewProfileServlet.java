a2c94024e8e58bc163cb789b082e7609c4bbb682
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-06-17 14:16:45 -0500

    pushing up work

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -1,5 +1,7 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,5 +17,7 @@ public class ViewProfileServlet extends HttpServlet {
             return;
         }
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        request.setAttribute("users", DaoFactory.getUsersDao().findByUsername("user"));
+
     }
 }
